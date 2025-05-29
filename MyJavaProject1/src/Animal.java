6fcd3697524baa4668a07c618d9a46a9003691f1
khalid-9j <khalidnaeemshaikh@gmail.com>
2025-05-29 14:45:53 +0530

    first commit

---
+++ MyJavaProject1/src/Animal.java
@@ -0,0 +1,25 @@
+
+public class Animal {
+	String habitat;
+	String sound;
+	double weight;
+	
+	public Animal(String habitat, String sound, double weight) {
+		this.habitat = habitat;
+		this.sound = sound;
+		this.weight = weight;
+	}
+	
+	public Animal() {}
+	
+@Override
+	public String toString() {
+		return "Animal [habitat=" + habitat + ", sound=" + sound + ", weight=" + weight + "]";
+	}
+
+public static void main (String args []) {
+	Animal dog = new Animal("street", "bow-bow", 20);
+
+	System.out.println(dog.toString());
+}
+}
